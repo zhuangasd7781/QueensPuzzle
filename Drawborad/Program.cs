00bb5e63b598cf35b62037c184cd38c6739fb0e6
John <zhuangasd7781@gmail.com>
2023-12-24 15:01:46 +0800

    加入專案檔案。

---
+++ Drawborad/Program.cs
@@ -0,0 +1,140 @@
+﻿using Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drawborad
+{
+    internal class Program
+    { 
+        static int b_size = 4;
+        static int[] qPosition = new int[2];
+        //static List<string[]> solutions = new List<string[]>();
+        static void Main(string[] args)
+        {
+            //回傳list 每個list有Q的座標
+
+            QueensPuzzleLogic logic = new QueensPuzzleLogic();
+            List<string> val = logic.Start(b_size);
+
+            while (true)
+            {
+                Console.Write("請輸入棋盤大小: ");
+
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out b_size) && b_size > 0)
+                {
+                    //Console.Write("請輸入要放置 'Q' 的座標（例如 2,2 2,3）: ");
+                    int i = 1;
+                    foreach (string v in val)
+                    {
+                        bool chk_xy_result = 檢查座標集合(v, out List<string[]> 皇后座標);
+                        if (chk_xy_result)
+                        {
+                            Console.WriteLine($"解方 {i} :");
+                            建置棋盤(b_size, 皇后座標);
+                        }
+
+                        Console.WriteLine();
+                        i += 1;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("請輸入有效的數字！");
+                }
+            }
+        }
+        static bool 檢查座標集合(string xy, out List<string[]> 皇后座標)
+        {
+            皇后座標 = new List<string[]>();
+            string[] qPositions = xy.Split(' ');
+
+            foreach (string p in qPositions)
+            {
+                string[] parts = p.Split(',');
+                if (parts.Length == 2 && int.TryParse(parts[0], out int row) && int.TryParse(parts[1], out int col))
+                {
+                    if (row > 0 && row <= b_size && col > 0 && col <= b_size)
+                    {
+                        皇后座標.Add(parts);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"無效座標: {p}");
+                        return false;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"無法解析座標: {p}");
+                    return false;
+
+                }
+            }
+            return true;
+
+        }
+        static void 建置棋盤(int row, List<string[]> 皇后座標)
+        {
+            if (row == 0)
+            {
+                繪製頂部數字();
+                return;
+            }
+
+            建置棋盤(row - 1, 皇后座標);
+            繪製棋格與皇后(true, row, 皇后座標);
+            繪製棋格與皇后(false, row, 皇后座標);
+
+            if (row == b_size)
+            {
+                繪製棋格與皇后(true, row, 皇后座標);
+            }
+        }
+        static void 繪製棋格與皇后(bool isEdge, int row, List<string[]> 皇后座標)
+        {
+            Console.Write(!isEdge ? $"{row} " : $"  ");
+
+            for (int col = 1; col <= b_size; col++)
+            {
+                if (!isEdge)
+                {
+                    bool isQueenPlaced = false;
+                    foreach (string[] p in 皇后座標)
+                    {
+                        int Q_Row = int.Parse(p[0]);
+                        int Q_Col = int.Parse(p[1]);
+                        if (row == Q_Row && col == Q_Col)
+                        {
+                            Console.Write("| Q ");
+                            isQueenPlaced = true;
+                            break;
+                        }
+                    }
+
+                    if (!isQueenPlaced)
+                    {
+                        Console.Write("|   ");
+                    }
+                }
+                else
+                {
+                    Console.Write("+---");
+                }
+            }
+            Console.WriteLine(isEdge ? "+" : "|");
+        }
+        static void 繪製頂部數字()
+        {
+            Console.Write("  "); // 頂部數字的起始空格
+            for (int i = 1; i <= b_size; i++)
+            {
+                Console.Write($"  {i} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Logic/QueensPuzzleLogic.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class QueensPuzzleLogic
+    {
+        int Size = 0;
+        bool[,] board;
+        List<string[]> solutions = new List<string[]>();
+        public QueensPuzzleLogic()
+        {
+        }
+
+        public List<string> Start(int _Size)
+        {
+            Size = _Size;
+            //board = new bool[Size, Size];
+            //
+            //Solve(0);
+            //PrintSolutions();
+
+            List<string> list = new List<string>();
+            list.Add("1,2 2,4 3,1 4,3");
+            list.Add("1,3 2,1 3,4 4,2");
+            return list;
+        }
+
+        void Solve(int row)
+        {
+            if (row == Size)
+            {
+                AddSolution();
+                return;
+            }
+
+            for (int col = 0; col < Size; col++)
+            {
+                if (IsSafe(row, col))
+                {
+                    board[row, col] = true;
+                    Solve(row + 1);
+                    board[row, col] = false;
+                }
+            }
+        }
+
+        bool IsSafe(int row, int col)
+        {
+            for (int i = 0; i < row; i++)
+            {
+                if (board[i, col])
+                    return false;
+
+                int diagLeft = col - (row - i);
+                if (diagLeft >= 0 && board[i, diagLeft])
+                    return false;
+
+                int diagRight = col + (row - i);
+                if (diagRight < Size && board[i, diagRight])
+                    return false;
+            }
+            return true;
+        }
+
+        void AddSolution()
+        {
+            string[] snapshot = new string[Size];
+            for (int i = 0; i < Size; i++)
+            {
+                snapshot[i] = "";
+                for (int j = 0; j < Size; j++)
+                    snapshot[i] += board[i, j] ? "Q" : "-";
+            }
+            solutions.Add(snapshot);
+        }
+
+        void PrintSolutions()
+        {
+            int count = 1;
+            foreach (var solution in solutions)
+            {
+                Console.WriteLine($"Solution {count++}:");
+                foreach (string row in solution)
+                {
+                    Console.WriteLine(row);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
